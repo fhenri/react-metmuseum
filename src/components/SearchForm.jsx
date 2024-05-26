2f501f237d238a236b824608971bf27ed31ac26b
fhenri <fhenri@fhsolution.com>
2024-05-26 16:04:13 +0200

    fix design

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useContext } from 'react';
 import { useParams } from "react-router-dom";
 import AdvancedSearch from './AdvancedSearch';
-import MuseumLogo from './M-full-01.svg';
+import MuseumLogo from './M-01.svg';
 
 function SearchForm ( props ) {
 
@@ -55,9 +55,9 @@ function SearchForm ( props ) {
                 <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                     <img src={ MuseumLogo } className="h-12" alt="Search" />                
                 </div>
-                <input type="text" id="query" name="query" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-20 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} value={query} onChange={(e) => setQuery(e.target.value)} required />
+                <input type="text" id="query" name="query" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full ps-20 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500" placeholder={placeholder} value={query} onChange={(e) => setQuery(e.target.value)} required />
                     </div>
-                    <button type="submit" className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <button type="submit" className="p-2.5 ms-2 text-sm font-medium text-white bg-red-700 rounded-lg border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
                         <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                         </svg>
