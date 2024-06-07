5430bcbafb13821006ef39415a4a1ca9543f8839
fhenri <fhenri@fhsolution.com>
2024-06-07 10:13:32 +0200

    fix padding between elements after we had title on top of image

--- src/components/ObjectCard.jsx
+++ src/components/ObjectCard.jsx
@@ -28,10 +28,10 @@ function ObjectCard (props) {
                 by {mObject.artistDisplayName}
             </h5> }
             <div className='h-full'></div>
-            <Link to={`/object/${objectId}`} state={ mObject } className='inline-flex font-medium text-red-met hover:underline text-end'>
-                View Object Details
-                <svg className="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
+            <Link to={`/object/${objectId}`} state={ mObject } className='inline-flex font-medium text-red-met hover:underline text-end pt-4'>
+                <span>View Object Details</span>
+                <svg className="w-6 h-6 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
                 </svg>
             </Link>
         </div>)
