f9d6bdcb837f999aff57e8a1b3bf961841a9080f
fhenri <fhenri@fhsolution.com>
2024-06-07 06:30:50 +0200

    add script to build on cloudflare

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/react-metmuseum/"
-})
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    base: env.BASE_DIRECTORY
+  }
+  })
+
